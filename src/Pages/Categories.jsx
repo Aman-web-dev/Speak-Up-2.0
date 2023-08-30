46b16de274fbfd7a99857d64909d0e8d1d34a4ff
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-08-30 16:24:26 +0530

    Time for a Change

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -37,12 +37,26 @@ export default function Categories({src, name}) {
     <h2 className="text-3xl font-bold mb-4 text-black">Top Languages</h2>
     <div className="card-container rounded-md">
         {flagData.map((data) => (
-            <div className="card card-compact min-w-[8rem] w-40 bg-base-100 shadow-md rounded-lg hover:shadow-2xl" key={data.id}>
-                <figure className='pt-8 my-auto'><img src={data.imgLink} alt={data.name} className='w-full' /></figure>
-                <div className="card-body flex items-center justify-between">
-                    <h2 className="card-title m-auto">{data.name}</h2>
-                </div>
-            </div>
+
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 shadow-xl my-2 xl:transform xl:hover:-translate-y-2 xl:transition-transform" key={data.id}>
+<a href="#">
+    <img class="rounded-t-lg" src={data.imgLink} alt={data.name} />
+</a>
+<div class="p-5">
+    <a href="#">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{data.name}</h5>
+    </a>
+   
+    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Read more
+         <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+    </a>
+</div>
+</div>
+
+           
         ))}
     </div>
 </section>
@@ -51,3 +65,7 @@ export default function Categories({src, name}) {
 
   )
 }
+
+
+
+
--- src/components/BigCard.jsx
+++ src/components/BigCard.jsx
@@ -4,7 +4,7 @@ function BigCard() {
   return (
     <div>
       
-<div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-auto">
     <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
         <li className="mr-2">
             <button id="about-tab" data-tabs-target="#about" type="button" role="tab" aria-controls="about" aria-selected="true" className="inline-block p-4 text-blue-600 rounded-tl-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-blue-500">About</button>
