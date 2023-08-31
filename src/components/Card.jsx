e219b1b422dcc7ea3ae145ac666e2d284e24ea34
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-08-31 23:12:06 +0530

    Card Okay

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import image from '../assets/img1.svg'
-
+import React from 'react';
+import image from '../assets/img1.svg';
 
 function Card(props) {
-
   return (
- 
-      
-<div className="xl:max-w-[25vw] my-2 m-1 h-auto max-w-sm border border-gray-200 rounded-lg shadow  dark:border-gray-700 xl:transform xl:hover:-translate-y-2 xl:transition-transform bg-gray-200 m-auto">
-    <a href="# " className=''>
-        <img className=" rounded-t-lg h-[60%] " src={props.pic} alt="product image" />
-    </a>
+    <div className="max-w-full mx-auto my-2 p-2 border border-gray-200 rounded-lg shadow bg-gray-200 xl:transform xl:hover:-translate-y-2 xl:transition-transform bg-gray-200 m-auto">
+      <a href="#">
+        <img className="w-full rounded-t-lg h-[60%]" src={props.pic} alt="product image" />
+      </a>
 
-    <div className="px-5 pb-5">
+      <div className="p-3">
         <a href="#">
-            <h5 className="text-l md:text-xl sm:text-l bg:text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{props.course}</h5>
+          <h5 className="text-lg font-semibold text-gray-900 dark:text-white">{props.course}</h5>
         </a>
-        <div className="flex items-center mt-2.5 mb-5">
-            <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+        <div className="flex items-center space-x-1 mt-2 mb-3">
+        <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
             </svg>
             <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
@@ -33,16 +29,15 @@ function Card(props) {
                 <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
             </svg>
             <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">{props.rating}</span>
+
         </div>
-        <div className="flex flex-col sm:flex-col md:flex-col bg:flex-col xl:flex-row items-center justify-between">
-            <span className="text-3xl font-bold text-gray-900 dark:text-white">₹: {props.price}</span>
-            <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        <div className="flex items-center justify-between">
+          <span className="text-lg font-bold text-gray-900 dark:text-white">₹: {props.price}</span>
+          <a href="#" className="px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-2 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
         </div>
+      </div>
     </div>
-</div>
-
-
-  )
+  );
 }
 
-export default Card
+export default Card;
