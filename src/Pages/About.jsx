3e150bdbf85c48f66a47d3c40c35b806cdf3a086
Aman-web-dev <amanzhx1234+github@gmail.com>
2023-10-26 12:37:13 +0530

    Changes 12:37 by aman on 26 oct

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -22,20 +22,20 @@ function About() {
             </div>
 
 
-            <div className='div-1  flex flex-col xl:flex-row lg:flex-col sm:flex-col item-center justify-center text-black'>
+            <div className='div-1 my-5 flex flex-col xl:flex-row lg:flex-col sm:flex-col item-center justify-center text-black'>
               
 
                 <div className='flex flex-col item-center justify-center'>
 
 
-                    <h2 className="text-4xl font-extrabold  text-blue-400">Title 1 will come Here</h2>
+                    <h2 className="text-4xl font-extrabold  text-blue-400">Title 2 will come Here</h2>
 
 
                     <p className='my-4 mx-4 text-center item-center justify-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus totam quisquam nam fuga deleniti eos magni corporis sapiente dolorum natus amet quis at ut reiciendis culpa numquam fugiat, nesciunt atque, expedita necessitatibus porro illum. Facere dignissimos culpa modi labore repellat totam ut. Nam, ad quos totam iste quisquam corporis aut! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatibus deserunt officia placeat asperiores autem numquam consequatur suscipit alias eos nobis doloremque, magnam laborum cumque aliquid porro tempore quidem quasi, dolorem incidunt consectetur assumenda. Ex atque ducimus vel quis maxime!</p>
                 </div>
 
 
-                <img className='m-4 xl:h-[50vh] rounded-lg w-[90vw] xl:mx-4 xl:my-4 m-auto' src="https://img.freepik.com/free-photo/business-women-signature-document_1388-90.jpg?w=900&t=st=1698056532~exp=1698057132~hmac=058c1a2c02042c183b469ff34afa73f2f561af06866727ce75e6c9b8513fd596" alt="img-1" />
+                <img className='m-4  xl:h-[50vh] rounded-lg w-[90vw] xl:mx-4 xl:my-4 m-auto' src="https://img.freepik.com/free-photo/business-women-signature-document_1388-90.jpg?w=900&t=st=1698056532~exp=1698057132~hmac=058c1a2c02042c183b469ff34afa73f2f561af06866727ce75e6c9b8513fd596" alt="img-1" />
             </div>
 
 
@@ -45,7 +45,7 @@ function About() {
                 <div className='flex flex-col item-center justify-center'>
 
 
-                    <h2 className="text-4xl font-extrabold  text-blue-400">Title 1 will come Here</h2>
+                    <h2 className="text-4xl font-extrabold  text-blue-400">Title 3 will come Here</h2>
 
 
                     <p className='my-4 mx-4 text-center item-center justify-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus totam quisquam nam fuga deleniti eos magni corporis sapiente dolorum natus amet quis at ut reiciendis culpa numquam fugiat, nesciunt atque, expedita necessitatibus porro illum. Facere dignissimos culpa modi labore repellat totam ut. Nam, ad quos totam iste quisquam corporis aut! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatibus deserunt officia placeat asperiores autem numquam consequatur suscipit alias eos nobis doloremque, magnam laborum cumque aliquid porro tempore quidem quasi, dolorem incidunt consectetur assumenda. Ex atque ducimus vel quis maxime!</p>
@@ -59,7 +59,7 @@ function About() {
                 <div className='flex flex-col item-center justify-center'>
 
 
-                    <h2 className="text-4xl font-extrabold  text-blue-400">Title 1 will come Here</h2>
+                    <h2 className="text-4xl font-extrabold  text-blue-400">Title 4 will come Here</h2>
 
 
                     <p className='my-4 mx-4 text-center item-center justify-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus totam quisquam nam fuga deleniti eos magni corporis sapiente dolorum natus amet quis at ut reiciendis culpa numquam fugiat, nesciunt atque, expedita necessitatibus porro illum. Facere dignissimos culpa modi labore repellat totam ut. Nam, ad quos totam iste quisquam corporis aut! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus, voluptatibus deserunt officia placeat asperiores autem numquam consequatur suscipit alias eos nobis doloremque, magnam laborum cumque aliquid porro tempore quidem quasi, dolorem incidunt consectetur assumenda. Ex atque ducimus vel quis maxime!</p>
